104cf993d6f0d92543d0de56e17e62564f33a378
MaorM <maormenashe2@gmailcom>
2024-06-11 18:22:21 +0300

    Add validate

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { FormikHelpers, useFormik } from "formik";
 import * as React from "react";
 
 interface IYoutubeFormProps {}
@@ -9,16 +9,40 @@ type YoutubeForm = {
   channel: string;
 };
 
+type YoutubeFormErrors = Partial<YoutubeForm>;
+
+const initialValues: YoutubeForm = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (
+  values: YoutubeForm,
+  formikHelpers: FormikHelpers<YoutubeForm>
+) => {
+  console.log("Submitted", values, formikHelpers);
+};
+
+const validate = (values: YoutubeForm) => {
+  const errors: YoutubeFormErrors = {};
+
+  if (!values.name) errors.name = "Required";
+
+  if (!values.email) errors.email = "Required";
+  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))
+    errors.email = "Invalid email address";
+
+  if (!values.channel) errors.channel = "Required";
+
+  return errors;
+};
+
 const YoutubeForm: React.FunctionComponent<IYoutubeFormProps> = () => {
   const formik = useFormik<YoutubeForm>({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit(values, formikHelpers) {
-      console.log("Submitted", values, formikHelpers);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   return (
@@ -51,7 +75,9 @@ const YoutubeForm: React.FunctionComponent<IYoutubeFormProps> = () => {
           value={formik.values.channel}
         ></input>
 
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={formik.isSubmitting}>
+          Submit
+        </button>
       </form>
     </div>
   );
