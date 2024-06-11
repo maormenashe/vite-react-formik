bf62035f52de263a3f0efd6dff5e93e7af2c85ac
MaorM <maormenashe2@gmailcom>
2024-06-11 14:14:14 +0300

    Add Formik & Manage Form State & Handling Submit

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,19 +1,55 @@
+import { useFormik } from "formik";
 import * as React from "react";
 
 interface IYoutubeFormProps {}
 
+type YoutubeForm = {
+  name: string;
+  email: string;
+  channel: string;
+};
+
 const YoutubeForm: React.FunctionComponent<IYoutubeFormProps> = () => {
+  const formik = useFormik<YoutubeForm>({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit(values, formikHelpers) {
+      console.log("Submitted", values, formikHelpers);
+    },
+  });
+
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name"></input>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        ></input>
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email"></input>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        ></input>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel"></input>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        ></input>
 
         <button>Submit</button>
       </form>
