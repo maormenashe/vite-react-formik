5c82aba5ba52354a6a89717fb2502eb3c1613c29
MaorM <maormenashe2@gmailcom>
2024-06-11 18:33:37 +0300

    Displaying Error Message

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -48,32 +48,47 @@ const YoutubeForm: React.FunctionComponent<IYoutubeFormProps> = () => {
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          onChange={formik.handleChange}
-          value={formik.values.name}
-        ></input>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          ></input>
+          {formik.errors.name && (
+            <div className="error">{formik.errors.name}</div>
+          )}
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        ></input>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          ></input>
+          {formik.errors.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          name="channel"
-          onChange={formik.handleChange}
-          value={formik.values.channel}
-        ></input>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            value={formik.values.channel}
+          ></input>
+          {formik.errors.channel && (
+            <div className="error">{formik.errors.channel}</div>
+          )}
+        </div>
 
         <button type="submit" disabled={formik.isSubmitting}>
           Submit
